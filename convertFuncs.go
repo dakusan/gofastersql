3d08830a3454ba341f4a658f5b9fd7c1b38ce4f9
Jeffrey Riaboy <dakusan@castledragmire.com>
2023-12-22 19:16:57 -0600

    Moved most conversion functions onto one line and added section comments

--- convertFuncs.go
+++ convertFuncs.go
@@ -8,6 +8,8 @@ import (
 	"unsafe"
 )
 
+//--------------------------Generic numeric converters--------------------------
+
 func convUNum[T uint8 | uint16 | uint32 | uint64](in []byte, p unsafe.Pointer, bits int) error {
 	if in == nil {
 		*(*T)(p) = 0
@@ -39,42 +41,24 @@ func convFloat[T float32 | float64](in []byte, p unsafe.Pointer, bits int) error
 	return nil
 }
 
-func convUint8(in []byte, p unsafe.Pointer) error {
-	return convUNum[uint8](in, p, 8)
-}
-func convUint16(in []byte, p unsafe.Pointer) error {
-	return convUNum[uint16](in, p, 16)
-}
-func convUint32(in []byte, p unsafe.Pointer) error {
-	return convUNum[uint32](in, p, 32)
-}
-func convUint64(in []byte, p unsafe.Pointer) error {
-	return convUNum[uint64](in, p, 64)
-}
-func convInt8(in []byte, p unsafe.Pointer) error {
-	return convINum[int8](in, p, 8)
-}
-func convInt16(in []byte, p unsafe.Pointer) error {
-	return convINum[int16](in, p, 16)
-}
-func convInt32(in []byte, p unsafe.Pointer) error {
-	return convINum[int32](in, p, 32)
-}
-func convInt64(in []byte, p unsafe.Pointer) error {
-	return convINum[int64](in, p, 64)
-}
-func convString(in []byte, p unsafe.Pointer) error {
-	*(*string)(p) = string(in)
-	return nil
-}
+//-------------------Conversion function for all scalar types-------------------
+
+func convUint8(in []byte, p unsafe.Pointer) error    { return convUNum[uint8](in, p, 8) }
+func convUint16(in []byte, p unsafe.Pointer) error   { return convUNum[uint16](in, p, 16) }
+func convUint32(in []byte, p unsafe.Pointer) error   { return convUNum[uint32](in, p, 32) }
+func convUint64(in []byte, p unsafe.Pointer) error   { return convUNum[uint64](in, p, 64) }
+func convInt8(in []byte, p unsafe.Pointer) error     { return convINum[int8](in, p, 8) }
+func convInt16(in []byte, p unsafe.Pointer) error    { return convINum[int16](in, p, 16) }
+func convInt32(in []byte, p unsafe.Pointer) error    { return convINum[int32](in, p, 32) }
+func convInt64(in []byte, p unsafe.Pointer) error    { return convINum[int64](in, p, 64) }
+func convFloat32(in []byte, p unsafe.Pointer) error  { return convFloat[float32](in, p, 32) }
+func convFloat64(in []byte, p unsafe.Pointer) error  { return convFloat[float64](in, p, 64) }
+func convString(in []byte, p unsafe.Pointer) error   { *(*string)(p) = string(in); return nil }
+func convRawBytes(in []byte, p unsafe.Pointer) error { *(*sql.RawBytes)(p) = in; return nil }
 func convByteArray(in []byte, p unsafe.Pointer) error {
 	out := make([]byte, len(in))
 	copy(out, in)
 	*(*[]byte)(p) = out
-	return nil
-}
-func convRawBytes(in []byte, p unsafe.Pointer) error {
-	*(*sql.RawBytes)(p) = in
 	return nil
 }
 func convBool(in []byte, p unsafe.Pointer) error {
@@ -84,10 +68,4 @@ func convBool(in []byte, p unsafe.Pointer) error {
 		*(*bool)(p) = in[0] == '1'
 	}
 	return nil
-}
-func convFloat32(in []byte, p unsafe.Pointer) error {
-	return convFloat[float32](in, p, 32)
-}
-func convFloat64(in []byte, p unsafe.Pointer) error {
-	return convFloat[float64](in, p, 64)
 }
