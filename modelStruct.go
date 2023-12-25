911cf7398ebf4d2d86dd90a0268691ba9b737b7c
Jeffrey Riaboy <dakusan@castledragmire.com>
2023-12-24 22:31:51 -0600

    Added section comments Added comments for members of StructModel and its children Renamed nullTypeStructs to nullTypeStructConverters Added scalarConverters, so reflect.Type -> converterFunc lookup is now done through an array instead of a switch statement

--- modelStruct.go
+++ modelStruct.go
@@ -13,29 +13,36 @@ import (
 	"unsafe"
 )
 
+//---------------------------Primary models and cache---------------------------
+
 // StructModel holds the model of a structure for processing as a RowReader. StructModel is concurrency safe.
 type StructModel struct {
-	fields   []structField
-	pointers []structPointer
-	rType    reflect.Type
+	fields   []structField   //The flattened list of members from a recursive structure search
+	pointers []structPointer //Data for structure pointers (recursive)
+	rType    reflect.Type    //The type of the top level structure. Used for lookup in remStructs
 }
-type converterFunc func(in []byte, p upt) error
 type structField struct {
-	offset       uintptr
-	converter    converterFunc
-	pointerIndex int
-	name         string
-	isPointer    bool
+	offset       uintptr       //The offset of the member in structure pointed at by RowReader.pointers[pointerIndex] (which is derived from StructModel.pointers)
+	converter    converterFunc //The conversion function
+	pointerIndex int           //The structure index to be used for offset (RowReader.pointers[pointerIndex], which is derived from StructModel.pointers)
+	name         string        //The recursed name of the member
+	isPointer    bool          //If the member is a pointer
 }
 type structPointer struct {
-	parentIndex int
-	offset      uintptr
-	name        string
+	parentIndex int     //The structure index to be used for offset (RowReader.pointers[parentIndex], which is derived from StructModel.pointers)
+	offset      uintptr //The offset of the member in structure pointed at by RowReader.pointers[parentIndex] (which is derived from StructModel.pointers)
+	name        string  //The recursed name of the member
 }
 
 // Store structs for future lookups
 var remStructs = make(map[reflect.Type]StructModel)
-var nullTypeStructs = map[reflect.Type]converterFunc{
+var remLock sync.RWMutex
+
+//-----------------------Mappings for conversion functions----------------------
+
+type converterFunc func(in []byte, p upt) error
+
+var nullTypeStructConverters = map[reflect.Type]converterFunc{
 	reflect.TypeOf(nulltypes.NullUint8{}):     cvNU8,
 	reflect.TypeOf(nulltypes.NullUint16{}):    cvNU16,
 	reflect.TypeOf(nulltypes.NullUint32{}):    cvNU32,
@@ -52,7 +59,42 @@ var nullTypeStructs = map[reflect.Type]converterFunc{
 	reflect.TypeOf(nulltypes.NullBool{}):      cvNB,
 	reflect.TypeOf(nulltypes.NullTime{}):      cvNT,
 }
-var remLock sync.RWMutex
+var scalarConverters = make([]converterFunc, reflect.UnsafePointer) //UnsafePointer is the final enum of reflect.Kind
+func init() {
+	for _, d := range []struct {
+		k reflect.Kind
+		f converterFunc
+	}{
+		{reflect.String, convString},
+		{reflect.Int, cond(unsafe.Sizeof(0) == unsafe.Sizeof(int32(0)), convInt32, convInt64)},
+		{reflect.Uint, cond(unsafe.Sizeof(uint(0)) == unsafe.Sizeof(uint32(0)), convUint32, convUint64)},
+		{reflect.Int8, convInt8},
+		{reflect.Int16, convInt16},
+		{reflect.Int32, convInt32},
+		{reflect.Int64, convInt64},
+		{reflect.Uint8, convUint8},
+		{reflect.Uint16, convUint16},
+		{reflect.Uint32, convUint32},
+		{reflect.Uint64, convUint64},
+		{reflect.Float32, convFloat32},
+		{reflect.Float64, convFloat64},
+		{reflect.Bool, convBool},
+	} {
+		if int(d.k) > len(scalarConverters) {
+			panic("reflect.UnsafePointer is no longer the highest reflect.Kind?")
+		}
+		scalarConverters[d.k] = d.f
+	}
+}
+
+var lookupType = struct{ time, nullInherit, byteArray, rawBytes reflect.Type }{
+	reflect.TypeOf(time.Time{}),
+	reflect.TypeOf(nulltypes.NullInherit{}),
+	reflect.TypeOf([]byte{}),
+	reflect.TypeOf(sql.RawBytes{}),
+}
+
+//------------------------------Create StructModels-----------------------------
 
 // ModelStruct extracts the model of a structure for processing as a RowReader.
 // This is just a wrapper for ModelStructType.
@@ -75,19 +117,19 @@ func ModelStructType(t reflect.Type) (StructModel, error) {
 	}
 	remLock.RUnlock()
 
-	//Function to determine if a struct is considered a scalar type
-	var isNullInheritType, isScalarStruct func(reflect.Type) bool
-	{
-		nullInheritType := reflect.TypeOf(nulltypes.NullInherit{})
-		timeType := reflect.TypeOf(time.Time{})
-		isNullInheritType = func(t reflect.Type) bool {
-			return t.NumField() > 0 && t.Field(0).Type == nullInheritType
-		}
-		isScalarStruct = func(t reflect.Type) bool {
-			return isNullInheritType(t) || t == timeType
-		}
-	}
+	return createStructModelFromStruct(t)
+}
 
+// Function to determine if a struct is considered a scalar type
+func isScalarStruct(t reflect.Type) bool {
+	return isNullInheritType(t) || t == lookupType.time
+}
+func isNullInheritType(t reflect.Type) bool {
+	return t.NumField() > 0 && t.Field(0).Type == lookupType.nullInherit
+}
+
+// Create a StructModel
+func createStructModelFromStruct(t reflect.Type) (StructModel, error) {
 	//Do a recursive count of the number of fields
 	numFields := 1
 	numStructPointers := 0
@@ -116,7 +158,6 @@ func ModelStructType(t reflect.Type) (StructModel, error) {
 		var processStruct func(reflect.Type, uintptr, int, string) []string
 		fieldPos := 0
 		structPointerPos := 0
-		byteArrayType, rawBytesType := reflect.TypeOf([]byte{}), reflect.TypeOf(sql.RawBytes{})
 		processStruct = func(v reflect.Type, parentOffset uintptr, parentStructIndex int, parentName string) (retErr []string) {
 			for i := 0; i < v.NumField(); i++ {
 				//Handle pointers
@@ -128,51 +169,8 @@ func ModelStructType(t reflect.Type) (StructModel, error) {
 				}
 
 				//Get the function pointer for the type
-				var fn converterFunc
-				switch fldType.Kind() {
-				case reflect.String:
-					fn = convString
-				case reflect.Slice:
-					if fldType.AssignableTo(byteArrayType) {
-						fn = cond(fldType == rawBytesType, convRawBytes, convByteArray)
-					}
-				case reflect.Int:
-					fn = cond(fldType.Size() == unsafe.Sizeof(int32(0)), convInt32, convInt64)
-				case reflect.Uint:
-					fn = cond(fldType.Size() == unsafe.Sizeof(uint32(0)), convUint32, convUint64)
-				case reflect.Int8:
-					fn = convInt8
-				case reflect.Int16:
-					fn = convInt16
-				case reflect.Int32:
-					fn = convInt32
-				case reflect.Int64:
-					fn = convInt64
-				case reflect.Uint8:
-					fn = convUint8
-				case reflect.Uint16:
-					fn = convUint16
-				case reflect.Uint32:
-					fn = convUint32
-				case reflect.Uint64:
-					fn = convUint64
-				case reflect.Float32:
-					fn = convFloat32
-				case reflect.Float64:
-					fn = convFloat64
-				case reflect.Bool:
-					fn = convBool
-				case reflect.Struct:
-					//Check for scalar structs
-					if isScalarStruct(fldType) {
-						if isNullInheritType(fldType) {
-							fn = nullTypeStructs[fldType]
-						} else {
-							fn = convTime
-						}
-						break
-					}
-
+				fn := scalarToConversionFunc(fldType)
+				if fn == nil && fldType.Kind() == reflect.Struct {
 					//Pointers to structures need to add their StructModel.pointers and redirect appropriately
 					offset, structIndex := parentOffset+fld.Offset, parentStructIndex
 					if isPointer {
@@ -211,6 +209,33 @@ func ModelStructType(t reflect.Type) (StructModel, error) {
 	//Return success
 	return ret, nil
 }
+
+// Convert a scalar reflect.Type to its conversion function
+func scalarToConversionFunc(fldType reflect.Type) converterFunc {
+	//Handle real scalar types
+	k := fldType.Kind()
+	cf := scalarConverters[k]
+	if cf != nil {
+		return cf
+	}
+
+	//Handle pretend scalar types
+	switch k {
+	case reflect.Slice:
+		if fldType.AssignableTo(lookupType.byteArray) {
+			return cond(fldType == lookupType.rawBytes, convRawBytes, convByteArray)
+		}
+	case reflect.Struct:
+		if isScalarStruct(fldType) {
+			return cond(isNullInheritType(fldType), nullTypeStructConverters[fldType], convTime)
+		}
+	}
+
+	//Return no match
+	return nil
+}
+
+//-------------------------------------Misc-------------------------------------
 
 // Equals returns if these are from the same struct
 func (sm StructModel) Equals(sm2 StructModel) bool {
